81295d4d58a7be7fba8a3c04ba14a93dae6761dc
Edson0710 <edsonnavarro0710@gmail.com>
2020-07-13 18:37:07 -0500

    Avance login

--- app/src/main/java/com/example/ezservice/MainActivity.java
+++ app/src/main/java/com/example/ezservice/MainActivity.java
@@ -6,19 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_registro;
+    TextView tv_registro;
+    EditText et_email, et_password;
+    Button btn_signup;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_registro = findViewById(R.id.btn_registrarse);
+        tv_registro = findViewById(R.id.tv_registro);
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        btn_signup = findViewById(R.id.btn_signup);
 
-        btn_registro.setOnClickListener(new View.OnClickListener() {
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, ""+et_email.getText().toString()+"  " + et_password.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        tv_registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(MainActivity.this, RegistrosOpciones.class);
