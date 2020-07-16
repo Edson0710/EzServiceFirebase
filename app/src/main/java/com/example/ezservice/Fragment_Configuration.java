857215de3817ac518974675c71b90876ea25ba60
luisfgaravil <luisfgaravil@gmail.com>
2020-07-16 01:23:32 -0500

    TabLaoyout agregada

---
+++ app/src/main/java/com/example/ezservice/Fragment_Configuration.java
@@ -0,0 +1,41 @@
+package com.example.ezservice;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Fragment_Configuration extends Fragment {
+    View view;
+    Button signout;
+
+    public Fragment_Configuration() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_configuration,container,false);
+
+        //Relaciones------------
+        signout=view.findViewById(R.id.btn_signout);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/ezservice/MainFeed.java
@@ -0,0 +1,101 @@
+package com.example.ezservice;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainFeed extends AppCompatActivity {
+
+    TextView btn_NombreApp;
+    ImageView btn_messages,btn_search;
+    TabLayout tabLayout;
+    int[] tabIcons={R.drawable.ic_search,R.drawable.ic_profile,R.drawable.ic_configuration};
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        //Relaciones------------------------
+        btn_NombreApp = findViewById(R.id.btn_nombreApp);
+        btn_messages = findViewById(R.id.btn_messages);
+        btn_search = findViewById(R.id.btn_search);
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.ly_TabLayout);
+
+        //Carga de ViewPager
+        loadViewPager(tabLayout);
+
+        //Carga de íconos
+        tabIcons(tabLayout);
+
+        initializeIconColors(tabLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                iconColor(tab,"#63a4ff");
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                iconColor(tab,"#E0E0E0");
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        btn_messages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainFeed.this, "Ir a mensajes", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainFeed.this, "Ir a búsqueda", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadViewPager(TabLayout tablayout){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment_Solicitar(),"");
+        adapter.addFragment(new Fragment_Lista(),"");
+        adapter.addFragment(new Fragment_Configuration(),"");
+        viewPager.setAdapter(adapter);
+        tablayout.setupWithViewPager(viewPager);
+    }
+
+    private void tabIcons(TabLayout tablayout){
+        for(int i=0;i<3;i++){
+            tablayout.getTabAt(i).setIcon(tabIcons[i]);
+            iconColor(tablayout.getTabAt(i),"#E0E0E0");
+        }
+    }
+
+    private void iconColor(TabLayout.Tab tab, String color){
+        tab.getIcon().setColorFilter(Color.parseColor(color), PorterDuff.Mode.SRC_IN);
+    }
+
+    private void initializeIconColors(TabLayout tablayout){
+        iconColor(tablayout.getTabAt(tabLayout.getSelectedTabPosition()),"#63a4ff");
+    }
+}
